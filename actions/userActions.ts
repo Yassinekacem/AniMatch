f8b5b5846a9d3328c0256094f18385a50b6981fa
Yassinekacem <kacemyassine740@gmail.com>
2024-07-28 15:36:28 +0100

    add use server

--- actions/userActions.ts
+++ actions/userActions.ts
@@ -1,18 +1,11 @@
 "use server";
+import {eq} from "drizzle-orm";
+import {revalidatePath} from "next/cache";
 import {db} from "@/db/drizzle";
-import {  users } from "@/db/schema";
-import { asc } from 'drizzle-orm';
-import { eq } from 'drizzle-orm';
-import { revalidatePath } from "next/cache";
+import {users} from "@/db/schema"; 
+import { clerkClient } from "@clerk/nextjs/server";
 
-export const getUserlById = async (id: number) => {
-  const result = await db.select().from(users).where(eq(users.id, id)).limit(1);
-  if (result.length > 0) {
-    return true
-  } else {
-    return false
-  }
-};
+
 
 export const getUser = async (userId: any) => {
 const user = await db.query.users.findMany(
@@ -45,53 +38,3 @@ export const addUser = async (user : any) => {
   .returning({clerkClientId : users?.clerkId})
   // revalidatePath("/");
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-export const getData = async () => {
-  const data = await db.select().from(users).orderBy(asc(users.id));
-  return data;
-};
-
-
-
-
-
-
-export const editUser = async (user : any , id : number) => {
-  await db
-    .update(users)
-    .set({
-      clerkId : user?.clerkId,
-      email : user?.email,
-      name : user?.name!,  
-      firstName : user?.firstName,  
-      lastName : user?.lastName,
-      photo : user?.photo
-    })
-    .where(eq(user.id, id));
-    revalidatePath("/");
-
-};
- 
-
-
-
-
-
-export const deleteUser = async (id: number) => {
-  await db.delete(users).where(eq(users.id, id));
-  revalidatePath("/");
-
-};
-
---
+++ types/userType.ts
@@ -0,0 +1,12 @@
+export type todoType = {
+    id: number;
+    name: string;
+    clerId: string;
+    firstName: string;
+    lastName : string;
+    photo : string;
+    email: string;
+    created_at: Date;
+    updatedAt: Date;
+
+  };
