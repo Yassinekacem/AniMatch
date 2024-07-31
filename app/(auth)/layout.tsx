ba09289ba2060cdf44ee88f57f79683b96dc6cd2
raefka <raefkacem20@gmail.com>
2024-07-31 18:25:31 +0100

    navbar and some changes

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function RootLayout({ children }:Readonly<{ children: React.ReactNode }>) {
+  return (
+   <main className="relative h-screen w-full">
+    <div className="absolute size-full ">
+       <Image src="/images/fond.jpg" alt="background" fill className="size-full"  />
+    </div>
+    {children}
+   </main> 
+  )
+}
