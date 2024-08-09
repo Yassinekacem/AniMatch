cd2487698404de615bb9035510ebd7dfe1b94d31
Yassinekacem <kacemyassine740@gmail.com>
2024-08-09 15:57:05 +0100

    add animal in db

--- app/(root)/announcement/page.tsx
+++ app/(root)/announcement/page.tsx
@@ -4,9 +4,14 @@ import Filter from '@/components/Filter';
 import Card from '@/components/Card';
 import { animalType } from '@/types/animalType';
 import axios from 'axios';  
-const Announcement = () => {
-  const [animals, setAnimals] = useState([]);
+const Announcement = () => { 
 
+  
+
+
+
+  // fetch animals from the server
+  const [animals, setAnimals] = useState([]); 
   const getAnimals = async () => {
     try {
       const response = await axios.get("http://localhost:3000/api/animals");
@@ -17,7 +22,20 @@ const Announcement = () => {
   };
   useEffect(() => {
     getAnimals();
-  }, []); 
+  }, []);   
+
+
+  // pagination 
+  const items = 5 
+  const [current, setCurrent] = useState(1); 
+  const nbPages = Math.ceil(animals.length / items);  
+
+  const startIndex = (current - 1) * items;  
+  const endIndex = startIndex + items; 
+  const dataPerPage = animals.slice(startIndex, endIndex); 
+
+
+
   return (
     <div className='flex flex-row mt-[20px]'>
       <div className='flex-1 '>
@@ -25,7 +43,7 @@ const Announcement = () => {
       </div>
 
       <div className='flex flex-2 gap-8 flex-wrap w-full'>
-        {animals.map((item : animalType) => <Card item={item} key={item.id} />)}
+        {dataPerPage.map((item : animalType) => <Card item={item} key={item.id} />)}
       </div>
     </div>
   );
