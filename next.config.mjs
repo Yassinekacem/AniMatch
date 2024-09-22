e945b608f57ddd2fcd98374d0b8f2490ce6d90a5
Yassinekacem <kacemyassine740@gmail.com>
2024-09-22 17:31:11 +0100

    enable cors2

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { 
     images: { 
-        remotePatterns : [
+        remotePatterns: [
             {
-                protocol : 'https',
-                hostname : "images.pexels.com",
-            },{
-                protocol : 'https',
-                hostname : "res.cloudinary.com",
-            },{
-                protocol : 'https',
-                hostname : "img.clerk.com",
-            } ,
-            
-
+                protocol: 'https',
+                hostname: "images.pexels.com",
+            },
+            {
+                protocol: 'https',
+                hostname: "res.cloudinary.com",
+            },
+            {
+                protocol: 'https',
+                hostname: "img.clerk.com",
+            },
         ]
-     } 
+    } 
+};
 
-}; 
-
-module.exports = {
+export default {
+    ...nextConfig,
     async headers() {
-      return [
-        {
-          // matching all API routes
-          source: "/api/:path*",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "*" },
-            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          ]
-        }
-      ]
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ],
+            }
+        ];
     }
-  };
-  
-
-
-export default nextConfig;
+};
